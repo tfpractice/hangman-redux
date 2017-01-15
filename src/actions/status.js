72d8e0ae3b19f27521fb380322fee0b44d3082d7
theory friction practice <jackson.brebnor@gmail.com>
2017-01-15 18:48:08 -0500

    resets on playGame

--- src/actions/status.js
+++ src/actions/status.js
@@ -1,12 +1,17 @@
 import { SET_STATUS, STATUS_ACTIONS, } from '../constants';
-
+import { resetGuesses, } from './guesses';
+import { resetRem, } from './remaining';
+import { resetAnimals, } from './animal';
 const set = over => status => ({ over, });
 
 export const endGame = () =>
 ({ type: SET_STATUS, curry: set(true), });
 
-export const playGame = () =>
-({ type: SET_STATUS, curry: set(false), });
-
+export const playGame = () => (dispatch) => {
+  dispatch({ type: SET_STATUS, curry: set(false), });
+  dispatch(resetGuesses());
+  dispatch(resetRem());
+  dispatch(resetAnimals());
+};
 export const setStatus = over =>
 ({ type: SET_STATUS, curry: set(over), });
